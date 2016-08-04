d01b22c880c40414d553c36791dde1dea60586be
ckoshien <ckoshien@gmail.com>
2016-08-04 20:40:45 +0900

    #4 データ入力時のプルダウンでJCBL管轄球団の優先度を下げる を修正。 SQLの修正によってfix

--- JCBLScore/src/main/resources/cx/myhome/ckoshien/sql/TeamDate.sql
+++ JCBLScore/src/main/resources/cx/myhome/ckoshien/sql/TeamDate.sql
@@ -6,10 +6,10 @@ inner join
   t.team_id as team_id,
   t.team_name as team_name,
   t.short_name as short_name,
-  max(game_date) as last_join_date
+   max(game_date) as last_join_date
  FROM team t
   left outer join result r on t.team_id=r.team_id
   left outer join game g on r.game_id=g.game_id
 group by team_id
-order by last_join_date desc) team_date
+order by jcbl_flg asc,last_join_date desc) team_date
 on p.team_id=team_date.team_id;
