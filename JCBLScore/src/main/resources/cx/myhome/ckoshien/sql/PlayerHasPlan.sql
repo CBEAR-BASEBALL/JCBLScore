7ecc1c7f4a2890414d6aebfd8e25274ebfe9d228
ckoshien <ckoshien@gmail.com>
2017-01-26 20:37:25 +0900

    修正

--- JCBLScore/src/main/resources/cx/myhome/ckoshien/sql/PlayerHasPlan.sql
+++ JCBLScore/src/main/resources/cx/myhome/ckoshien/sql/PlayerHasPlan.sql
@@ -1,18 +1,6 @@
 select
   *
 from player p
-inner join
-(SELECT
-  t.team_id as team_id,
-  t.team_name as team_name,
-  t.short_name as short_name,
-   max(game_date) as last_join_date
- FROM team t
-  left outer join result r on t.team_id=r.team_id
-  left outer join game g on r.game_id=g.game_id
-  group by team_id
-order by jcbl_flg asc,last_join_date desc) team_date
-on p.team_id=team_date.team_id
 inner join (
 SELECT
  p.id
@@ -23,3 +11,4 @@ inner JOIN team te ON te.team_id=p.team_id
 group by player_id
 ) m
 on m.id=p.id
+order by team_id
